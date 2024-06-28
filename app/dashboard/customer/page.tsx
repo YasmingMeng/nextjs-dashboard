c7e3f294d4e71b375b49c380ce4fa8cea61e021d
YasmingMeng <1661237617@qq.com>
2024-06-28 14:28:24 +0800

    next项目初始化

---
+++ app/dashboard/customer/page.tsx
@@ -0,0 +1,10 @@
+/*
+ * @Description: 
+ * @Date: 2024-06-27 15:06:24
+ * @LastEditTime: 2024-06-27 16:29:43
+ */
+import '@/app/ui/global.css';
+
+export default function Page() {
+  return <p className="bg-black text-blue bg-blue-300">This is customers Page</p>;
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+/*
+ * @Description: 
+ * @Date: 2024-06-25 17:42:22
+ * @LastEditTime: 2024-06-27 12:06:23
+ */
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({weight: '400', subsets: ['latin'] });
