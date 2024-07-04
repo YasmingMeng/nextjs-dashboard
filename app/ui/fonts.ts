d0f629366c3254f3977062235826c07b0ccc4651
YasmingMeng <1661237617@qq.com>
2024-07-04 14:52:19 +0800

    修复自体错误

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,10 +1,13 @@
 /*
  * @Description: 
  * @Date: 2024-06-25 17:42:22
- * @LastEditTime: 2024-06-27 12:06:23
+ * @LastEditTime: 2024-06-28 14:45:33
  */
 import { Inter, Lusitana } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 
-export const lusitana = Lusitana({weight: '400', subsets: ['latin'] });
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
